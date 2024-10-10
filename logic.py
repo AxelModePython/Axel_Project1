69f29ba3b1907f3cce0ad0345258672e86611e7f
AxelModePython <axelcisprabaswara@gmail.com>
2024-10-10 18:16:47 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -11,7 +11,6 @@ def gen_emodji():
     emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
     return random.choice(emodji)
 
-
 def flip_coin():
     flip = random.randint(0, 2)
     if flip == 0:
